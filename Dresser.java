8e7e61e9501d9493feca892090d3ff1b24170bb4
Brennan Johnson <brennanMJ123@gmail.com>
2021-11-02 09:34:36 -0400

    Add files via upload

---
+++ Dresser.java
@@ -0,0 +1,15 @@
+/*
+ * Brennan Johnson
+ */
+package homework;
+
+public class Dresser 
+{
+	private Clothing[][] clothes;
+	
+	public Dresser()
+	{
+		this.clothes = new Clothing[5][10];
+	}
+	
+}
